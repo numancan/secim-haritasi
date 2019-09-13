106743b338628fca4dfa85e50b26ed8963144cb8
Numan Can İpekçi <n.canipekci@gmail.com>
2019-09-13 03:07:38 +0300

    Updates

---
+++ precache-manifest.f142463ffb7a86f711f993ba8a92d210.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e755d9f2a2952aedd5125299b0759b",
+    "url": "/secim-haritasi/index.html"
+  },
+  {
+    "revision": "c16a7ba39392536244f8",
+    "url": "/secim-haritasi/static/css/main.005f510b.chunk.css"
+  },
+  {
+    "revision": "2b845b3d0d447cb384f6",
+    "url": "/secim-haritasi/static/js/2.33b32cc8.chunk.js"
+  },
+  {
+    "revision": "c16a7ba39392536244f8",
+    "url": "/secim-haritasi/static/js/main.871ac4a2.chunk.js"
+  },
+  {
+    "revision": "a2e586ea4f1817f8a9c6",
+    "url": "/secim-haritasi/static/js/runtime~main.a18b7624.js"
+  }
+]);
