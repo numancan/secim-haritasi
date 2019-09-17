6dfdefa8045df96e979bddc87965d6c1a23332b6
Numan Can İpekçi <n.canipekci@gmail.com>
2019-09-18 01:10:28 +0300

    deleted unused component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import Detail from './components/Detail';
 import { ProvinceProvider } from './ProvinceContext';
 import GeneralResult from './components/GeneralResult';
 import styles from './App.module.css';
-import RandomResults from './components/RandomResults';
 
 const App = () => {
   return (
     <ProvinceProvider className="helo">
-      <RandomResults />
       <div className={styles.container}>
         <GeneralResult />
         <Map />
